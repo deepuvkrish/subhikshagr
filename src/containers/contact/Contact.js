7d5bf8ef4f647346f406fa77f797162dfe06be59
Deepak K V <deepak.k@skillassure.com>
2024-06-16 17:40:09 +0530

    formal 1st commit

--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -1,60 +1,75 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "./Contact.scss";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
-import {illustration, contactInfo} from "../../portfolio";
-import {Fade} from "react-reveal";
-import email from "../../assets/lottie/email";
+import { illustration, contactInfo } from "../../portfolio";
+import { Fade } from "react-reveal";
+import codingPerson from "../../assets/lottie/codingPerson";
+import greenconsultation from "../../assets/lottie/Greenconsultation";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Contact() {
-  const {isDark} = useContext(StyleContext);
+  const { isDark } = useContext(StyleContext);
   return (
     <Fade bottom duration={1000} distance="20px">
       <div className="main contact-margin-top" id="contact">
         <div className="contact-div-main">
+
           <div className="contact-header">
-            <h1 className="heading contact-title">{contactInfo.title}</h1>
-            <p
-              className={
-                isDark
-                  ? "dark-mode contact-subtitle"
-                  : "subTitle contact-subtitle"
-              }
-            >
-              {contactInfo.subtitle}
-            </p>
-            <div
-              className={
-                isDark ? "dark-mode contact-text-div" : "contact-text-div"
-              }
-            >
-              {contactInfo.number && (
-                <>
-                  <a
-                    className="contact-detail"
-                    href={"tel:" + contactInfo.number}
-                  >
-                    {contactInfo.number}
-                  </a>
-                  <br />
-                  <br />
-                </>
+            <div className="lotte-contact">
+              {illustration.animated ? (
+                <DisplayLottie animationData={greenconsultation} />
+              ) : (
+                <img
+                  alt="Man Working"
+                  src={require("../../assets/images/developerActivity.svg")}
+                ></img>
               )}
-              <a
-                className="contact-detail-email"
-                href={"mailto:" + contactInfo.email_address}
+            </div>
+            <div className="contact-section">
+              <h1 className="heading contact-title">{contactInfo.title}</h1>
+              <p
+                className={
+                  isDark
+                    ? "dark-mode contact-subtitle"
+                    : "subTitle contact-subtitle"
+                }
               >
-                {contactInfo.email_address}
-              </a>
-              <br />
-              <br />
-              <SocialMedia />
+                {contactInfo.subtitle}
+              </p>
+              <div
+                className={
+                  isDark ? "dark-mode contact-text-div" : "contact-text-div"
+                }
+              >
+                {contactInfo.number && (
+                  <>
+                    <a
+                      className="contact-detail"
+                      href={"tel:" + contactInfo.number}
+                    >
+                      {contactInfo.number}
+                    </a>
+                    <br />
+                    <br />
+                  </>
+                )}
+                <a
+                  className="contact-detail-email"
+                  href={"mailto:" + contactInfo.email_address}
+                >
+                  {contactInfo.email_address}
+                </a>
+                <br />
+                <br />
+                <SocialMedia />
+
+              </div>
             </div>
           </div>
           <div className="contact-image-div">
             {illustration.animated ? (
-              <DisplayLottie animationData={email} />
+              <DisplayLottie animationData={greenconsultation} />
             ) : (
               <img
                 alt="Man working"
